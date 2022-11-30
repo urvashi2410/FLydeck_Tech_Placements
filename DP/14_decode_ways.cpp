a534f9760d743e48d85645c9775604a58c86c37a
urvashi2410 <urvashianand0906@gmail.com>
2022-12-01 00:38:41 +0530

    corrected decode ways

--- DP/14_decode_ways.cpp
+++ DP/14_decode_ways.cpp
@@ -3,30 +3,35 @@
 #include<string>
 using namespace std;
 
-int main(){
-    string str;
-    cin >> str;
+int decodeWays(string str){
     vector<int>dp(str.size());
+    int ans;
     dp[0] = 1;
 
     for (int i = 1; i < str.length(); i++) {
         if (str[i - 1] == '0' && str[i] == '0') {
-            dp[i] = 0;
+            dp[i] = i >= 2 ? dp[i - 2] : 1;
         } 
         else if (str[i - 1] == '0' && str[i] != '0') {
             dp[i] = dp[i - 1];
         } 
         else if (str[i - 1] != '0' && str[i] == '0') {
-            if (str[i - 1] == '1' || str[i - 1] == '2') {
-                dp[i] = (i >= 2 ? dp[i - 2] : 1);
-            }
+            dp[i] = dp[i - 2];
         } 
         else {
-            dp[i] = dp[i - 1];
-            if (stoi(str.substr(i - 1, i + 1)) <= 26) {
-                dp[i] += (i >= 2 ? dp[i - 2] : 1);
+            if(stoi(str.substr(i - 1, i + 1)) <= 26){
+                dp[i] = dp[i - 1] + (i >= 2 ? dp[i - 2] : 1);
+            }
+            else{
+                dp[i] = dp[i - 1];
             }
         }
     }
-    cout << dp[str.length() - 1];
+    return dp[str.length() - 1];
+}
+int main(){
+    string str;
+    cin >> str;
+    
+    cout << decodeWays(str);
 }
